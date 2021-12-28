774a071700b633db749fc1ab241472f2fb588d3c
Corentin.Vives <corentin.vives@insa-rennes.fr>
2021-12-28 15:32:38 +0100

    ordering aspects

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,11 +2,12 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
 
 	//@Before("execution(public void add*())")
@@ -17,25 +18,8 @@ public class MyDemoLoggingAspect {
 		System.out.println("\n ======>>> Executing @Before advice on a method");
 	}*/
 	
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
-	private void forDaoPackage() {}
-	
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
-	private void getter() {}
-	
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
-	private void setter() {}
-	
-	@Pointcut("forDaoPackage() && !(getter() || setter())")
-	private void forDaoPackageNoGetterSetter() {}
-	
-	@Before("forDaoPackageNoGetterSetter()")
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n ======>>> Executing @Before advice on a method");
-	}
-	
-	@Before("forDaoPackageNoGetterSetter()")
-	public void performApiAnalytics() {
-		System.out.println("\n ======>>> Performing API analytics");
 	}
 }
